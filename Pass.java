24cc457053192bb96546dc3b5d49c42c91203581
mayilms <mayilsamy.ms1418@gmail.com>
2017-06-12 12:16:04 +0530

    Create Pass.java

---
+++ Pass.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Pass
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.new int();
+if(n>0)
+{
+System.out.println("positive");
+}
+else if(n==0)
+{
+System.out.println("zero");
+}
+else
+{
+System.out.println("negative"):
+}
+}}
