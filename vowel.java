2e904fc409ab72c76f4cdea6548cfce39ae487f3
mayilms <mayilsamy.ms1418@gmail.com>
2017-06-13 10:05:52 +0530

    Create vowel.java

--- vowel.java
+++ vowel.java
@@ -3,10 +3,10 @@ public class Vowel
 {
 public static void main(String args[])
 {
-Scanner s=new Scanner();
+Scanner s=new Scanner(System.in);
 char a;
 String s1=s.nextLine();
-a=s1.chatAt(0);
+a=s1.charAt(0);
 if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u')
 {
 System.out.println("vowel");
