aaf8342aee85e6f9f9d86995340b4c37ed5c5cdc
mayilms <mayilsamy.ms1418@gmail.com>
2017-06-13 11:29:03 +0530

    Create Hello1.java

---
+++ Hello1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello1
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+for(int i=0;i<5;i++)
+{
+System.out.println(a);
+}
+}
+}
