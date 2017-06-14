b84e4481eb2a3f57012845a255913ee2d4906701
mayilms <mayilsamy.ms1418@gmail.com>
2017-06-14 09:42:36 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Natural
+{
+public static void main(String args[])
+{
+int sum=0;
+Scanner s=new Scanner(System.in);
+int a=s.new int();
+for(int i=1;i<=a;i++)
+{
+sum=sum+i;
+System.out.println(sum);
+}
+}
+}
+
