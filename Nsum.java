e4976249017a8b874576d3356b0e07ca38b86641
mayilms <mayilsamy.ms1418@gmail.com>
2017-06-14 09:46:41 +0530

    Create Nsum.java

---
+++ Nsum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Nsum
+{
+public static void main(String args[])
+{
+sum=0;
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(int i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.println(sum);
+}
+}
