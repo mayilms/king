cf552eaec9cb8a89492ad3d642788039d5dcfbdb
mayilms <mayilsamy.ms1418@gmail.com>
2017-06-23 10:05:54 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Power
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=new int();
+int p=new int();
+for(int i=1;i<p;i++)
+{
+int n1 n1=(n*n);
+System.out.println(n1);
+}
+}
+}
+
+
